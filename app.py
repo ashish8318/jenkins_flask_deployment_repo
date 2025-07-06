5a4ef4da85ace2718b493c370ea5afed18717035
Ashish Kumar <ashishyadavupc@gmail.com>
2025-07-06 22:14:48 +0530

    Testing argocd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # return "Hello from Flask on Kubernetes!"
-    return "Testing ngrock"
+    return "<h1>****** Welcome to ARGOCD *****"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
